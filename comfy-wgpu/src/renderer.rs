6087fae1762c7493e512d1ff43ac929ffb2c9ca8
Jakub Arnold <darthdeus@gmail.com>
2023-09-18 23:21:39 +0200

    Hide record-pngs under a flag

--- comfy-wgpu/src/renderer.rs
+++ comfy-wgpu/src/renderer.rs
@@ -1630,128 +1630,131 @@ impl WgpuRenderer {
             self.render_debug(&surface_view);
         }
 
+        #[cfg(feature = "record-pngs")]
         {
-            let mut encoder = self.context.device.create_command_encoder(
-                &wgpu::CommandEncoderDescriptor {
-                    label: Some("Copy output texture Encoder"),
-                },
-            );
-
-            encoder.copy_texture_to_buffer(
-                wgpu::ImageCopyTexture {
-                    texture: &output.texture,
-                    mip_level: 0,
-                    origin: wgpu::Origin3d::ZERO,
-                    aspect: wgpu::TextureAspect::All,
-                },
-                wgpu::ImageCopyBuffer {
-                    buffer: &self.screenshot_buffer.buffer,
-                    layout: wgpu::ImageDataLayout {
-                        offset: 0,
-                        bytes_per_row: Some(
-                            std::mem::size_of::<u32>() as u32 *
-                                self.config.width,
-                        ),
-                        rows_per_image: Some(self.config.height),
+            {
+                let mut encoder = self.context.device.create_command_encoder(
+                    &wgpu::CommandEncoderDescriptor {
+                        label: Some("Copy output texture Encoder"),
                     },
-                },
-                output.texture.size(),
-            );
+                );
 
-            self.context.queue.submit(std::iter::once(encoder.finish()));
-        }
+                encoder.copy_texture_to_buffer(
+                    wgpu::ImageCopyTexture {
+                        texture: &output.texture,
+                        mip_level: 0,
+                        origin: wgpu::Origin3d::ZERO,
+                        aspect: wgpu::TextureAspect::All,
+                    },
+                    wgpu::ImageCopyBuffer {
+                        buffer: &self.screenshot_buffer.buffer,
+                        layout: wgpu::ImageDataLayout {
+                            offset: 0,
+                            bytes_per_row: Some(
+                                std::mem::size_of::<u32>() as u32 *
+                                    self.config.width,
+                            ),
+                            rows_per_image: Some(self.config.height),
+                        },
+                    },
+                    output.texture.size(),
+                );
 
-        pollster::block_on(async {
-            let buffer_slice = self.screenshot_buffer.buffer.slice(..);
+                self.context.queue.submit(std::iter::once(encoder.finish()));
+            }
+
+            pollster::block_on(async {
+                let buffer_slice = self.screenshot_buffer.buffer.slice(..);
+
+                let (tx, rx) =
+                    futures_intrusive::channel::shared::oneshot_channel();
+
+                buffer_slice.map_async(wgpu::MapMode::Read, move |result| {
+                    tx.send(result).unwrap();
+                });
+
+                self.context.device.poll(wgpu::Maintain::Wait);
+                rx.receive().await.unwrap().unwrap();
+
+                let data = buffer_slice.get_mapped_range();
+
+                let path = std::env::current_exe().unwrap();
+                let example_name = path.file_name().unwrap().to_string_lossy();
+
+                let images_dir = format!("target/images/{}", example_name);
+
+                let videos_dir = "target/videos";
+
+                std::fs::create_dir_all(&images_dir).unwrap();
+                std::fs::create_dir_all(&videos_dir).unwrap();
+
+                let name =
+                    format!("{}/image-{:03}.png", &images_dir, get_frame());
+
+                {
+                    let mut rgba_data: Vec<u8> = Vec::with_capacity(data.len());
+
+                    for chunk in data.chunks_exact(4) {
+                        let b = chunk[0];
+                        let g = chunk[1];
+                        let r = chunk[2];
+                        let a = chunk[3];
+
+                        rgba_data.push(r);
+                        rgba_data.push(g);
+                        rgba_data.push(b);
+                        rgba_data.push(a);
+                    }
 
 
-            let (tx, rx) =
-                futures_intrusive::channel::shared::oneshot_channel();
+                    let mut buffer =
+                        image::ImageBuffer::<image::Rgba<u8>, _>::from_raw(
+                            self.config.width,
+                            self.config.height,
+                            rgba_data,
+                        )
+                        .unwrap();
 
-            buffer_slice.map_async(wgpu::MapMode::Read, move |result| {
-                tx.send(result).unwrap();
+                    buffer.save(name).unwrap();
+                }
+
+                if get_frame() > 60 {
+                    let ffmpeg_command = "ffmpeg";
+                    let framerate = "30";
+                    let input_pattern = "image-%03d.png";
+                    let output_file =
+                        format!("{}/{}.webm", videos_dir, example_name);
+
+                    let output = std::process::Command::new(ffmpeg_command)
+                        .arg("-framerate")
+                        .arg(framerate)
+                        .arg("-y")
+                        .arg("-i")
+                        .arg(format!("{}/{}", images_dir, input_pattern))
+                        .arg(output_file)
+                        .output()
+                        .expect("Failed to execute ffmpeg command");
+
+                    if output.status.success() {
+                        println!("Successfully generated the GIF.");
+                    } else {
+                        println!("Error generating the GIF:");
+                        println!(
+                            "stdout: {}",
+                            String::from_utf8_lossy(&output.stdout)
+                        );
+                        println!(
+                            "stderr: {}",
+                            String::from_utf8_lossy(&output.stderr)
+                        );
+                    }
+
+                    std::process::exit(0);
+                }
             });
 
-            self.context.device.poll(wgpu::Maintain::Wait);
-            rx.receive().await.unwrap().unwrap();
-
-            let data = buffer_slice.get_mapped_range();
-
-            let path = std::env::current_exe().unwrap();
-            let example_name = path.file_name().unwrap().to_string_lossy();
-
-            let images_dir = format!("target/images/{}", example_name);
-
-            let videos_dir = "target/videos";
-
-            std::fs::create_dir_all(&images_dir).unwrap();
-            std::fs::create_dir_all(&videos_dir).unwrap();
-
-            let name = format!("{}/image-{:03}.png", &images_dir, get_frame());
-
-            {
-                let mut rgba_data: Vec<u8> = Vec::with_capacity(data.len());
-
-                for chunk in data.chunks_exact(4) {
-                    let b = chunk[0];
-                    let g = chunk[1];
-                    let r = chunk[2];
-                    let a = chunk[3];
-
-                    rgba_data.push(r);
-                    rgba_data.push(g);
-                    rgba_data.push(b);
-                    rgba_data.push(a);
-                }
-
-
-                let mut buffer =
-                    image::ImageBuffer::<image::Rgba<u8>, _>::from_raw(
-                        self.config.width,
-                        self.config.height,
-                        rgba_data,
-                    )
-                    .unwrap();
-
-                buffer.save(name).unwrap();
-            }
-
-            if get_frame() > 60 {
-                let ffmpeg_command = "ffmpeg";
-                let framerate = "30";
-                let input_pattern = "image-%03d.png";
-                let output_file =
-                    format!("{}/{}.webm", videos_dir, example_name);
-
-                let output = std::process::Command::new(ffmpeg_command)
-                    .arg("-framerate")
-                    .arg(framerate)
-                    .arg("-y")
-                    .arg("-i")
-                    .arg(format!("{}/{}", images_dir, input_pattern))
-                    .arg(output_file)
-                    .output()
-                    .expect("Failed to execute ffmpeg command");
-
-                if output.status.success() {
-                    println!("Successfully generated the GIF.");
-                } else {
-                    println!("Error generating the GIF:");
-                    println!(
-                        "stdout: {}",
-                        String::from_utf8_lossy(&output.stdout)
-                    );
-                    println!(
-                        "stderr: {}",
-                        String::from_utf8_lossy(&output.stderr)
-                    );
-                }
-
-                std::process::exit(0);
-            }
-        });
-
-        self.screenshot_buffer.buffer.unmap();
+            self.screenshot_buffer.buffer.unmap();
+        }
 
         output.present();
     }
