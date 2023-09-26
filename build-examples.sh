aeafd66be5a6b548a3fbbd84a2397574d493a850
Jakub Arnold <darthdeus@gmail.com>
2023-09-26 18:38:23 +0200

    Build all examples in CI

--- build-examples.sh
+++ build-examples.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 
-mkdir -p target/generated/
-rm -rf target/generated/*
-
-EXAMPLE=sprite
-
-for example in $(echo $EXAMPLE); do
-  RUSTFLAGS=--cfg=web_sys_unstable_apis cargo build --target wasm32-unknown-unknown --release --example "$example" --features blobs
-  # cp -r examples/$1/resources target/generated/ || true
-  dir="target/generated/$example"
-  mkdir -p "$dir"
-  sed "s/{{example}}/$example/" > "$dir/index.html" < index.html
-  wasm-bindgen --out-dir "$dir" --target web "target/wasm32-unknown-unknown/release/examples/$example.wasm"
+for example in $(ls comfy/examples | grep -e "\.rs$" | sed "s/\.rs//"); do
+  cargo build --example "$example" --features blobs
 done
