cc773569d4b39e800179b9d9eae0425b578fc3fa
Jakub Arnold <darthdeus@gmail.com>
2023-09-29 22:58:53 +0200

    Working y-sort test

--- comfy/examples/y_sort.rs
+++ comfy/examples/y_sort.rs
@@ -8,4 +8,5 @@ fn setup(_c: &mut EngineContext) {
 
 fn update(_c: &mut EngineContext) {
     draw_comfy(Vec2::ZERO, WHITE, 0, splat(5.0));
+    draw_comfy(vec2(3.0, (4.0 * get_time()).sin() as f32), RED, 0, splat(3.0));
 }
