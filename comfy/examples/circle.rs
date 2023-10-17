c6635b6fa069a82086f87ff6ccdcdee8d35d278f
Jakub Arnold <darthdeus@gmail.com>
2023-10-17 14:45:02 +0200

    HDR circle

--- comfy/examples/circle.rs
+++ comfy/examples/circle.rs
@@ -1,7 +1,22 @@
-use comfy::{draw_circle, simple_game, vec2, EngineContext, RED};
+use comfy::*;
 
-simple_game!("Nice red circle", update);
+simple_game!("Nice red circle", setup, update);
+
+fn setup(_c: &mut EngineContext) {
+    // Enable bloom
+    game_config_mut().bloom_enabled = true;
+}
 
 fn update(_c: &mut EngineContext) {
-    draw_circle(vec2(0.0, 0.0), 0.5, RED, 0);
+    // Note the color is multiplied by 5.0 to make it brighter
+    // and glow with the bloom effect. This is possible because
+    // Comfy supports HDR.
+    draw_circle(vec2(0.0, 0.0), 0.5, RED * 5.0, 0);
+
+    draw_text(
+        "Nice red glowing circle with the help of HDR bloom",
+        vec2(0.0, -2.0),
+        WHITE,
+        TextAlign::Center,
+    );
 }
