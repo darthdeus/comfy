241f6d6d9ae3201080e534d7c133d78cae103cab
Jakub Arnold <darthdeus@gmail.com>
2023-11-05 16:05:44 +0100

    Add more info to the fragment shader example

--- comfy/examples/fragment-shader.rs
+++ comfy/examples/fragment-shader.rs
@@ -21,7 +21,13 @@ fn setup(_state: &mut GameState, _c: &mut EngineContext) {
 fn update(state: &mut GameState, c: &mut EngineContext) {
     if state.my_shader_id.is_none() {
         state.my_shader_id = Some(
-            // Shader requires a default value for every uniform
+            // Comfy now supports shader hot reloading. We'll create a simple shader and provide
+            // both `static_source` which would be used in release builds allowing the game to be
+            // shipped with shaders embedded in the binary, as well as a path for hot reloading,
+            // which will be watched by Comfy and hot-reloaded on change.
+            //
+            // Note that currently hot reloading an invalid shader will crash. This will get fixed
+            // relatively soon.
             create_reloadable_shader(
                 &mut c.renderer.shaders.borrow_mut(),
                 "my-shader",
@@ -31,8 +37,20 @@ fn update(state: &mut GameState, c: &mut EngineContext) {
                     )),
                     path: "comfy/examples/fragment-shader.wgsl".to_string(),
                 },
+                // Uniforms can have default values. When we switch to this shader we'll have to
+                // set all the uniforms that don't have a default value before drawing anything
+                // using the shader, otherwise we'll get a crash.
+                //
+                // In this case we don't provide a default for "time" as we'll set it every frame,
+                // but we will provide a value for "intensity" just to showcase how this would
+                // work.
+                //
+                // Experiment with this to learn what happens in different scenarios!
+                //
+                // If you change "intensity" default to `None` you'll get a crash saying which
+                // uniform was missing a value.
                 hashmap! {
-                    "time".to_string() => UniformDef::F32(Some(0.0)),
+                    "time".to_string() => UniformDef::F32(None),
                     "intensity".to_string() => UniformDef::F32(Some(1.0)),
                 },
             )
@@ -73,7 +91,7 @@ fn update(state: &mut GameState, c: &mut EngineContext) {
     set_uniform_f32("intensity", state.intensity);
 
     draw_comfy(vec2(2.0, 0.0), WHITE, 0, splat(1.0));
-    
+
     // We can also easily switch back to the default sprite shader.
     use_default_shader();
     draw_comfy(vec2(4.0, 0.0), WHITE, 0, splat(1.0));
