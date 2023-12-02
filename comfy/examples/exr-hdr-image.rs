61bd62a47750cd6e7cd717178860cacb97cf86bb
Jakub Arnold <darthdeus@gmail.com>
2023-12-02 14:27:10 +0100

    Add non-hdr image to exr test

--- comfy/examples/exr-hdr-image.rs
+++ comfy/examples/exr-hdr-image.rs
@@ -1,9 +1,16 @@
 use comfy::*;
 use comfy_core::image::GenericImageView;
 
+// This example requires the test images from https://github.com/sobotka/Testing_Imagery
+// to be available in a directory next to the Comfy repo (as a sibling). We don't embed these in
+// Comfy since the files are quite bit.
 simple_game!("EXR HDR Image", setup, update);
 
 const TEST_IMAGES: &[&str] = &[
+    // Compare to
+    // https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/TestScreen_square_more_colors.svg/1024px-TestScreen_square_more_colors.svg.png
+    "assets/color-test-1024px.png",
+    // All of these are from the Testing_Imagery repo linked at the top.
     "../Testing_Imagery/CC24_BT-709_uniform_1931.exr",
     "../Testing_Imagery/chinese_garden_2k.exr",
     "../Testing_Imagery/alexa_BT_709.exr",
@@ -12,33 +19,53 @@ const TEST_IMAGES: &[&str] = &[
 
 fn setup(c: &mut EngineContext) {
     for path in TEST_IMAGES {
-        // Test images from https://github.com/sobotka/Testing_Imagery
         let img =
             image::io::Reader::open(path).unwrap().decode().unwrap().flipv();
 
         let gc = &c.renderer.context;
         let label = *path;
 
-        let f16_data = img
-            .to_rgba32f()
-            .iter()
-            .copied()
-            .map(half::f16::from_f32)
-            .collect_vec();
+        let (img, texture) = if path.ends_with(".exr") {
+            let f16_data = img
+                .to_rgba32f()
+                .iter()
+                .copied()
+                .map(half::f16::from_f32)
+                .collect_vec();
 
-        let px_size = 4 * std::mem::size_of::<half::f16>();
+            let px_size = 4 * std::mem::size_of::<half::f16>();
 
-        let texture = Texture::from_image_data_with_format(
-            &gc.device,
-            &gc.queue,
-            bytemuck::cast_slice(&f16_data),
-            Some(label),
-            wgpu::AddressMode::ClampToEdge,
-            wgpu::TextureFormat::Rgba16Float,
-            img.dimensions(),
-            px_size as u32,
-        )
-        .unwrap();
+            let texture = Texture::from_image_data_with_format(
+                &gc.device,
+                &gc.queue,
+                bytemuck::cast_slice(&f16_data),
+                Some(label),
+                wgpu::AddressMode::ClampToEdge,
+                wgpu::TextureFormat::Rgba16Float,
+                img.dimensions(),
+                px_size as u32,
+            )
+            .unwrap();
+
+            (img, texture)
+        } else {
+            let data = img.to_rgba8();
+            let px_size = 4 * std::mem::size_of::<u8>();
+
+            let texture = Texture::from_image_data_with_format(
+                &gc.device,
+                &gc.queue,
+                bytemuck::cast_slice(&data),
+                Some(label),
+                wgpu::AddressMode::ClampToEdge,
+                wgpu::TextureFormat::Rgba8Unorm,
+                img.dimensions(),
+                px_size as u32,
+            )
+            .unwrap();
+
+            (img, texture)
+        };
 
         load_texture_with_image(
             &c.renderer.context,
@@ -77,11 +104,17 @@ fn update(_c: &mut EngineContext) {
 
     clear_background(Color::gray(0.1));
 
+    let size = if *CURRENT_IMAGE.lock() == 0 {
+        vec2(viewport.y, viewport.y)
+    } else {
+        viewport
+    };
+
     draw_sprite(
         texture_id(TEST_IMAGES[*CURRENT_IMAGE.lock()]),
         Vec2::ZERO,
         WHITE,
         1,
-        viewport,
+        size,
     );
 }
