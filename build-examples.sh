4cbb12e21bd9a24e3e6f35c9d2da957b12eb8d15
Jakub Arnold <darthdeus@gmail.com>
2023-12-09 10:39:39 +0100

    Fix build-examples

--- build-examples.sh
+++ build-examples.sh
@@ -2,5 +2,5 @@
 set -euxo pipefail
 
 for example in comfy/examples/*.rs; do
-  cargo build --example "$(basename -s .rs "${example}")" --features blobs,ldtk
+  cargo build --example "$(basename -s .rs "${example}")" --features blobs,ldtk,exr
 done
